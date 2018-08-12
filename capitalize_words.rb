23bb98054a0fcbc99a5ac16e8554d89870167015
Oshi99 <oshiryaeva@protonmail.com>
2018-08-12 11:40:46 -0400

    first commit

---
+++ capitalize_words.rb
@@ -0,0 +1,9 @@
+def word_cap(str)
+  str = str.split(' ')
+  str.each { |word| word[0] = word[0].upcase! if word[0] =~ /[[:alpha:]]/ }
+
+  str.join(' ')  
+end
+  
+puts word_cap('this is a "quoted" word')
+puts word_cap('the javaScript language')
